3fbdb082051ec47bcc87b8088c7c9982cc76e2ea
rogerndaba <rogerndaba@gmail.com>
2019-09-21 23:00:44 +0200

    some neatening

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -4,12 +4,12 @@
 	test
 */
 
-void print(char *s){printf(s,10,9,34,s);};
+void print(char *s) { printf(s,10,9,34,s); };
 
 int main(void){
 	/*
 		inside
 	*/
-	char *s="#include <stdio.h>%1$c%1$c/*%1$c%2$ctest%1$c*/%1$c%1$cvoid print(char *s){printf(s,10,9,34,s);};%1$c%1$cint main(void){%1$c%2$c/*%1$c%2$c%2$cinside%1$c%2$c*/%1$c%2$cchar *s=%3$c%4$s%3$c;%1$c%2$cprint(s);%1$c}%1$c";
+	char *s="#include <stdio.h>%1$c%1$c/*%1$c%2$ctest%1$c*/%1$c%1$cvoid print(char *s) { printf(s,10,9,34,s); };%1$c%1$cint main(void){%1$c%2$c/*%1$c%2$c%2$cinside%1$c%2$c*/%1$c%2$cchar *s=%3$c%4$s%3$c;%1$c%2$cprint(s);%1$c}%1$c";
 	print(s);
 }
